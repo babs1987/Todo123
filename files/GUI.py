25dcc0fccdd12572561d721f1b0470357a8c37c7
babs1987 <erlinyuriy@gmail.com>
2021-12-19 15:50:12 +0700

    Версия с сортировкой и объектами

--- files/GUI.py
+++ files/GUI.py
@@ -1,5 +1,5 @@
 import tkinter, files
-
+from operator import attrgetter
 
 
 class GUI:
@@ -8,17 +8,21 @@ class GUI:
         self.root = tkinter.Tk()
         self.root.geometry("800x600")
         self.b1 = tkinter.Button(text="Добавить задание", command=self.add_task)
-        self.b1.place(relx=0, rely=0,relwidth=0.25)
+        self.b1.place(relx=0, rely=0,relwidth=0.33)
         # self.b2 = tkinter.Button(text="Получить задание", command="")
         # self.b2.place(relx=0.25, rely=0,relwidth=0.25)
         self.b3 = tkinter.Button(text="Удалить задание по инд", command=self.del_by_index)
-        self.b3.place(relx=0.5, rely=0, relwidth=0.25)
+        self.b3.place(relx=0.33, rely=0, relwidth=0.33)
         self.b4 = tkinter.Button(text="Удалить выбранное задание", command=self.del_active)
-        self.b4.place(relx=0.75, rely=0, relwidth=0.25)
+        self.b4.place(relx=0.66, rely=0, relwidth=0.33)
         self.entry=tkinter.Entry()
         self.entry.place(relx=0.4,rely=0.1,relwidth=0.2)
-        self.lb=tkinter.Listbox()
-        self.lb.place(relx=0.4,rely=0.2,relwidth=0.2)
+        self.butSort=tkinter.Button(text="сортировка",command=self.sort)
+        self.butSort.place(relx=0.4, rely=0.2, relwidth=0.2)
+        self.butSort = tkinter.Button(text="выполнено/не выполнено", command=self.make_completed)
+        self.butSort.place(relx=0.4, rely=0.3, relwidth=0.2)
+        self.lb = tkinter.Listbox(self.root,selectbackground="RED")
+        self.lb.place(relx=0.4, rely=0.4, relwidth=0.2)
         self.fill_list_box()
 
     def add_task(self):
@@ -26,18 +30,40 @@ class GUI:
         self.diary.addtask(te)
         self.fill_list_box()
         self.entry.delete(0, tkinter.END)
+
     def fill_list_box(self):
         self.lb.delete(0, tkinter.END)
         for task in self.diary.tasks:
-            self.lb.insert(tkinter.END, task)
+            if task.completed == "False":
+                self.lb.insert(tkinter.END, task.task+" X")
+            else:
+                self.lb.insert(tkinter.END, task.task + " V")
+
+
+
     def del_by_index(self):
         ind=int(self.entry.get())
         self.diary.delete_task_by_index(ind)
         self.fill_list_box()
     def del_active(self):
+
         task=self.lb.get("active")
         self.diary.delete_task_by_task(task)
         self.fill_list_box()
+
+    def make_completed(self):
+
+        task = self.lb.get("active")
+        self.diary.make_completed_task(task)
+        self.fill_list_box()
+
+
+
+    def sort(self):
+        self.diary.tasks.sort(key=attrgetter("task"))
+
+        self.fill_list_box()
+
 
 yiii=GUI(files.Diary("ege.txt"))
 
--- files/files.py
+++ files/files.py
@@ -1,33 +1,75 @@
+class Task:
+    def __init__(self, task, completed="False"):
+        self.task = task
+        self.completed = completed
+
+    def __str__(self):
+        return f"задача: {self.task} завершенность:{self.completed}"
+
+
 class Diary:
     def __init__(self, path):
         self.path = path
+        self.tasks = []
         f = open(path, "r", encoding="UTF8")
-        self.tasks = f.read().split("|")
+        # print(f.read().split("|"))
+        for i in f.read().split("|"):
+            self.tasks.append(self.str_to_obj(i))
         f.close()
 
     def gettasks(self):
-        print("some")
         return self.tasks
 
     def addtask(self, task):
-        self.tasks.append(task)
-        self.update_file()
-    def update_file(self):
-        f = open(self.path, "w", encoding="utf8")
-        f.write("|".join(self.tasks))
-        f.close()
-    def delete_task_by_index(self,index):
-        self.tasks.pop(index)
-        self.update_file()
-    def delete_task_by_task(self,task):
-        self.tasks.remove(task)
+        self.tasks.append(Task(task))
         self.update_file()
 
-# s = Diary("ege.txt")
-# # st=input("Задача:\n")
-# # s.addtask(st)
+    def update_file(self):
+        f = open(self.path, "w", encoding="utf8")
+        f.write("|".join(self.get_tasks_by_obj()))
+
+    def obj_to_str(self,obj):
+        return f"{obj.task}<>{obj.completed}"
+
+    def str_to_obj(self, stry):
+        return Task(stry.split("<>")[0],stry.split("<>")[1])
+
+    def get_tasks_by_obj(self):
+        return [self.obj_to_str(i) for i in self.tasks]
+
+    def delete_task_by_index(self, index):
+        self.tasks.pop(index)
+        self.update_file()
+
+    def delete_task_by_task(self, task):
+        for i in range(len(self.tasks)):
+            if self.tasks[i].task==task[0:-2]:
+                self.tasks.pop(i)
+                return
+        self.update_file()
+
+    def make_completed_task(self, task):
+        for i in range(len(self.tasks)):
+            if self.tasks[i].task == task[0:-2] and self.tasks[i].completed=="False":
+                self.tasks[i].completed = "True"
+                return
+            elif self.tasks[i].task == task[0:-2] and self.tasks[i].completed=="True":
+                self.tasks[i].completed = "False"
+        self.update_file()
+
+s = Diary("ege.txt")
+# st=input("Задача:\n")
+# s.addtask(st)
+#
 # print(s.gettasks())
+#
 # # s.delete_task_by_index(3)
 # # print(s.gettasks())
-# s.delete_task_by_task("купить водку")
-# print(s.gettasks())
+# #
+# # print(s.gettasks())
+
+for i in s.tasks:
+    print(i.completed)
+
+for i in s.tasks:
+    print(i)
